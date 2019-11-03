3d1fd870bdf7f0218808f3eb509faf2a7418531b
sirDeniel <sirdeniel3@gmail.com>
2019-11-03 15:15:34 -0500

    Init commit, bizz84 work

---
+++ lib/app/sign_in/email_sign_in_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:time_tracker_flutter_course/app/sign_in/email_sign_in_form.dart';
+import 'package:time_tracker_flutter_course/services/auth.dart';
+
+class EmailSignInPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Sign in'),
+        elevation: 2.0,
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Card(
+            child: EmailSignInForm(),
+          ),
+        ),
+      ),
+      backgroundColor: Colors.grey[200],
+    );
+  }
+}
